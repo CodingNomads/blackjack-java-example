8e535c074675eaa9040cf9d2d918ef25cbfda124
Ryan Desmond <radesmond@gmail.com>
2020-06-22 14:33:49 -0700

    blackjack in Java - initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# A Basic Blackjack Game in Java
+
+## Deployment
+Import this project into IntelliJ and right click and "Run" on the "EntryPoint" class.
+
+## Built With
+* Java 8
+* IntelliJ IDE
+
+## Authors
+
+* **Ryan Desmond ** - [CodingNomads](https://codingnomads.co)
+
+## License
+
+This project is licensed under the MIT License - https://opensource.org/licenses/MIT
+
+## Acknowledgments
+* geeksforgeeks.com (always a great resource!)
+* Oracle Java docs
+
+
---
+++ src/main/java/blackjack/controllers/EntryPoint.java
@@ -0,0 +1,22 @@
+package main.java.blackjack.controllers;
+
+/**
+ * Created by ryandesmond - https://codingnomads.co
+ */
+public class EntryPoint {
+
+    /**
+     * This main() method is the entry point for the application
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        // create an object of the BlackJackGame class
+        BlackJackGame blackJackGame = new BlackJackGame();
+        // invoked the play() method in BlackJackGame
+        blackJackGame.play();
+
+    }
+
+}
